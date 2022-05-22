f91f7f3252e35a60c767626bfd2401b315e4728d
prlina01 <36077702+prlina01@users.noreply.github.com>
2022-05-22 23:59:00 +0200

    Fix NFT metadata url.

--- constants/nft.ts
+++ constants/nft.ts
@@ -2,4 +2,4 @@ import NFT from '../artifacts/contracts/NFT/CryptoDevsNFT.sol/CryptoDevsNFT.json
 
 export const NFT_CONTRACT_ADDRESS="0xF679F38C2d6D5da2350f4E50f6A75A46111A521d"
 export const ABI = NFT.abi
-export const METADATA_URL = "https://dao-flame.vercel.app/api/"
+export const METADATA_URL = "https://westpunks.vercel.app/api/"
